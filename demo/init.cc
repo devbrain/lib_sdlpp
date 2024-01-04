36a3f670da9d75091d85ef9ce50dc9845ff9cb64
Igor <igor.gutnik@gmail.com>
2024-01-04 08:59:37 +0200

    initial commit

---
+++ demo/init.cc
@@ -0,0 +1,11 @@
+//
+// Created by igor on 1/3/24.
+//
+
+#include <sdlpp/system.hh>
+
+int main([[maybe_unused]] int argc,[[maybe_unused]] char* argv[]) {
+  neutrino::sdl::system init(neutrino::sdl::init_flags::VIDEO, neutrino::sdl::init_flags::EVENTS, neutrino::sdl::init_flags::TIMER);
+  return 0;
+}
+
---
+++ src/sdlpp/system.cc
@@ -0,0 +1,12 @@
+//
+// Created by igor on 26/06/2021.
+//
+
+#include <sdlpp/system.hh>
+#include <sdlpp/surface.hh>
+#include <sdlpp/texture.hh>
+#include <sdlpp/timers.hh>
+#include <sdlpp/window.hh>
+#include <sdlpp/events/event_types.hh>
+#include <sdlpp/events/events_dispatcher.hh>
+#include <sdlpp/events/system_events.hh>
